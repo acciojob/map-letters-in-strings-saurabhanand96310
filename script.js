03f5e1aec201d41770b000fb02d90b46d2c1b68a
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-10-01 17:39:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const parseCode = (str) => {
+	let mapLetter={}
+	for(let i=0;i<str.length;i++){
+		let char=str.charAt(i);
+		if(!mapLetter[char]){
+			mapLetter[char]=[]
+		}
+		mapLetter[char].push(i);
+	}
+	return mapLetter;
+}
+const str = prompt("Enter str: ");
+alert(JSON.stringify(parseCode(str)));
